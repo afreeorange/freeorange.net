5532f550eb4164000bf7c7d69c69e27374fe715d
afreeorange <anand.nikhil@gmail.com>
2026-09-13 14:28:34 -0400

    v2 Black and Orange

--- src/app.tsx
+++ src/app.tsx
@@ -1,30 +1,27 @@
 const App = () => (
   <>
-    <content-top>
-      <header>
-        <h1>
-          <span>a free</span> Orange, <span>LLC</span>
-        </h1>
-      </header>
-      <main>
-        <p>
-          Is a boutique <strong>full-stack consultancy</strong> based in{" "}
-          <strong>Des Moines</strong>
-        </p>
-      </main>
-    </content-top>
-    <content-bottom>
-      <footer>
-        <p>
-          <a
-            href="mailto:hello@freeorange.net?subject=Hi there!"
-            title={"Email me!"}
-          >
-            Say hello
-          </a>
-        </p>
-      </footer>
-    </content-bottom>
+    <header>
+      <h1>
+        <span>a free</span> <span>O</span>range, <span>LLC</span>
+      </h1>
+    </header>
+    <main>
+      <p>
+        Is a <strong>full-stack consultancy</strong> based in
+        <br /> <strong>Des Moines</strong> <span>&amp;</span> <br />
+        <strong>New York City</strong>.
+      </p>
+    </main>
+    <footer>
+      <p>
+        <a
+          href="mailto:hello@freeorange.net?subject=Hi there!"
+          title={"Email me!"}
+        >
+          Say hello
+        </a>
+      </p>
+    </footer>
   </>
 );
 
